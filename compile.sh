bc73c8b45ea90fa48c77551e21de3a84b5ea0d5b
Alexandre Edeline <alexandre.edeline@MacBook-Pro-de-strafella.local>
2025-07-24 17:20:52 +0200

    First Commit

---
+++ compile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+CMD="latexmk -xelatex main.tex"
+CMD+=" && echo 'cleaning log files...' "
+CMD+=" && latexmk -c main.tex"
+CMD+=" && rm -f *.bbl && rm *.xml"
+CMD+=" && echo 'done.' "
+eval $CMD
