a5add2e52d9d5a49ca58e3fe6485419d98df5998
Ocelot-Pale <ocepa@proton.me>
2025-07-30 16:00:48 +0200

    analyse stabilité 2/... [début paramètres k,D]

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-CMD+=" latexmk -c main.tex && rm -f *.bbl && rm *.xml"
-CMD="latexmk -xelatex main.tex"
+CMD=" latexmk -c main.tex"
+CMD+="&& latexmk -xelatex main.tex"
 CMD+=" && echo 'cleaning log files...' "
 CMD+=" && latexmk -c main.tex"
 CMD+=" && rm -f *.bbl && rm *.xml"
 CMD+=" && echo 'done.' "
-eval $CMD
+eval $CMD   
